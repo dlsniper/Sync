dcf3a95a286e57d5ddafcc34a8a084d587e6f9b7
Florin Patan <florin@monzi7.(none)>
2011-12-24 17:29:39 +0200

    removed old version

--- v2/makedoc.sh
+++
@@ -1,1 +0,0 @@
-/home/florinp/subdomains/code/phpdoc/PhpDocumentor-1.4.3/phpdoc -o HTML:Smarty:default -d /home/florinp/taskserver/ -t /home/florinp/subdomains/code/docs/taskserver/ > /dev/null 2>&1
--- v2/srv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-clear
-rm -f /tmp/taskserver.sync.pid
-php server.php
